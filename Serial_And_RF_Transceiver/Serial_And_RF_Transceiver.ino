c1c604ae7f2fc14399d17e890017a87ff9487d7c
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-11-08 19:33:23 -0300

    change: receiving a string with Serial.read

---
+++ Serial_And_RF_Transceiver/Serial_And_RF_Transceiver.ino
@@ -0,0 +1,56 @@
+#include <VirtualWire.h>
+
+//===== Hardware Mapping ======//
+#define TX_Pin 12
+
+//===== Variables =====//
+#define MaxBufferSize 64
+byte TX_buffer[MaxBufferSize];
+
+void setup() {
+  Serial.begin(9600);
+
+  vw_set_tx_pin(TX_Pin);
+  vw_setup(4000);
+
+}
+
+void loop() {
+  clearBuffer();
+
+  if(Serial.available() > 0){
+    // Read the data sent by Rapberry via serial
+    // int bytesReadQuantity = Serial.readBytes(TX_buffer, MaxBufferSize);
+    String receivedString = Serial.readString();
+
+    int receivedInt = atoi(receivedString.c_str());
+    Serial.println(receivedInt);
+    
+    TX_buffer[0] = receivedInt;
+
+    int bytesReadQuantity = 1;
+  
+    //Check if the read data is correct  
+    if (bytesReadQuantity > 0) { //verify if the buffer size is greater than 0
+      Serial.println("Read Data: ");
+      for(int i = 0; i < bytesReadQuantity; i++){
+        Serial.print("Byte ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(TX_buffer[i]);
+      }
+    }
+    //Envia os dados pela RF com duas tentativas automáticas    
+    for(int i = 0; i < 2; i++){
+      vw_send(TX_buffer, MaxBufferSize);
+      vw_wait_tx();
+      delay(1000);
+    }
+  }
+}
+
+void clearBuffer(){
+  for(int i = 0; i < MaxBufferSize; i++){
+    TX_buffer[i] = 0;
+  }
+}
